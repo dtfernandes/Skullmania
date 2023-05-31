a1e2dbcba63ca8030a1dd40755458e644f026ada
unknown <csi@U-G06-PC13.ulht.gl.pt>
2023-05-31 18:13:41 +0100

    lamo lol even

--- Assets/Skull.cs
+++ Assets/Skull.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Skull : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
     private GameObject player;
+    [SerializeField]
+    private AudioSource sound;
+    [SerializeField]
+    private GameObject beep;
+
+    private MeshRenderer mesh;
 
     private void Start()
     {
+        mesh = GetComponent<MeshRenderer>();
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
@@ -17,10 +25,10 @@ public class Skull : MonoBehaviour
         if (player != null)
         {
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position + new Vector3(0,1,0), speed);
-            transform.LookAt(-player.transform.position);
+            //
+
+            //transform.LookAt(-player.transform.position); 
             //transform.eulerAngles += new Vector3(0, 100, 0);
-
-
         }
     }
 
@@ -30,7 +38,19 @@ public class Skull : MonoBehaviour
         if (other.CompareTag("Kill"))
         {
             Debug.Log("What are teh lel rules");
-            Destroy(gameObject);
+            sound.Play();
+            StartCoroutine("Kill");
+            mesh.enabled = false;
+            Destroy(beep);
         }
     }
+
+    IEnumerator Kill()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+        Destroy(sound);
+    }
 }
+
+
