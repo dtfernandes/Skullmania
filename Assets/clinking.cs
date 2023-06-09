bd010bb571f7ca58f2d4e7a3f898b3abadb32143
dtfernandes <danielteotonio@sapo.pt>
2023-06-09 16:03:59 +0100

    Cling

--- Assets/clinking.cs
+++ Assets/clinking.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(AudioSource))]
 public class clinking : MonoBehaviour
 {
+    [SerializeField]
+    private AudioSource _source;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _source = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        _source.Play();
     }
 }
